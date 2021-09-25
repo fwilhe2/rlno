dc604d176d2726310604341e8c6f6ed82c4b8ced
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-09-25 23:46:00 +0200

    Get player names per team

--- main.go
+++ main.go
@@ -36,15 +36,37 @@ func main() {
 		if s.HasClass("hauptlink") && s.HasClass("hide-for-small") {
 			teamLink, _ := s.Find("a").Attr("href")
 			fmt.Printf("Team link for %s: %s\n", s.Text(), teamLink)
-			players := []Player{}
-			// todo: follow team link, scrape players and fill for each team
+			players := scrapePlayers(teamLink)
 			teams = append(teams, Team{Name: strings.TrimSpace(s.Text()), Players: players})
 		}
 	})
 
-	println(len(teams))
+	fmt.Printf("Got %d teams", len(teams))
 
 	j, _ := json.MarshalIndent(teams, "", "\t")
 	fmt.Println(string(j))
 
+}
+
+func scrapePlayers(teamLink string) []Player {
+	resp, err := http.Get("https://www.transfermarkt.de" + teamLink)
+	if err != nil {
+		panic(err)
+	}
+
+	d, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	players := []Player{}
+
+	d.Find("#yw1 > table > tbody > tr > td").Each(func(i int, s *goquery.Selection) {
+		// is there a better way to identify attributes in the table?
+		if s.HasClass("hide") {
+			println(s.Text())
+			players = append(players, Player{Name: strings.TrimSpace(s.Text())})
+		}
+	})
+	return players
 }

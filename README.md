25084922a46b537f900ac05b7d36691e7e4351d6
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-07-06 22:21:48 +0200

    Simulate whole season

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rlno
+
+how to play
+
+```
+go run .
+npm ci
+npx ts-node game.ts
+```
--- game.ts
+++ game.ts
@@ -1,5 +1,11 @@
 import * as fs from 'fs';
 
+interface League {
+    Name: String
+    Teams: Array<Team>
+    Matches: Array<MatchData>
+}
+
 interface Team {
     Name: string
     Stats: Stats
@@ -16,30 +22,62 @@ interface Stats {
 }
 
 interface GameResult {
+    homeTeam: string
     homeGoals: number
+    awayTeam: string
     awayGoals: number
+    events: Array<string>
+}
+
+interface MatchData {
+    homeTeam: Team
+    awayTeam: Team
 }
 
 const teams: Array<Team> = JSON.parse(fs.readFileSync("data.json").toString())
 
-const teamHome = teams[0]
-const teamAway = teams[9]
+const game: MatchData = {
+    homeTeam: teams[0],
+    awayTeam: teams[9]
+}
 
-function playMatch() {
+class Match {
+    homeTeam: Team
+    awayTeam: Team
+
+    constructor(h: Team, a: Team) {
+        this.homeTeam = h
+        this.awayTeam = a
+    }
+}
+
+function calculateMatchesForSeason(teams: Array<Team>): Array<Match> {
+    return teams.flatMap(t => {
+        return teams.filter(tt => tt != t).map(ttt => new Match(t, ttt))
+
+    })
+
+}
+
+function playMatch(game: Match): GameResult {
+    console.log(`Game ${game.homeTeam.Name} vs ${game.awayTeam.Name}`)
     let minute = 0
     let result: GameResult = {
         homeGoals: 0,
-        awayGoals: 0
+        homeTeam: game.homeTeam.Name,
+        awayGoals: 0,
+        awayTeam: game.awayTeam.Name,
+        events: []
     }
 
     do {
         if (Math.random() < 0.01) {
             result.homeGoals++
-            console.log(`[${minute}]: Goal for ${teamHome.Name}, standings: ${result.homeGoals}:${result.awayGoals}`)
+            console.log(`[${minute}]: Goal for ${game.homeTeam.Name}, standings: ${result.homeGoals}:${result.awayGoals}`)
         }
         if (Math.random() < 0.01) {
             result.awayGoals++
-            console.log(`[${minute}]: Goal for ${teamAway.Name}, standings: ${result.homeGoals}:${result.awayGoals}`)
+            console.log(`[${minute}]: Goal for ${game.awayTeam.Name}, standings: ${result.homeGoals}:${result.awayGoals}`)
         }
 
         minute++
@@ -48,4 +86,27 @@ function playMatch() {
     return result
 }
 
-console.log(playMatch())
+// console.log(playMatch(game))
+
+const matches = calculateMatchesForSeason(teams)
+const results = matches.map(m => playMatch(m))
+fs.writeFileSync('match-results.json', JSON.stringify(results, null, 4))
+
+interface Standing {
+
+    team: string
+    points: Number
+}
+
+function calculateStandings(matchResults: Array<GameResult>): Array<Standing> {
+    return teams.map(t => { return { team: t.Name, points: calculateStandingsForTeam(t.Name, matchResults) } }).sort((a, b) => b.points - a.points)
+}
+
+function calculateStandingsForTeam(team: string, matchResults: Array<GameResult>) {
+    return matchResults.filter(m => m.homeTeam == team).filter(m => m.homeGoals > m.awayGoals).length * 3 +
+        matchResults.filter(m => m.awayTeam == team).filter(m => m.awayGoals > m.homeGoals).length * 3 +
+        matchResults.filter(m => m.homeTeam == team || m.awayTeam == team).filter(m => m.homeGoals === m.awayGoals).length * 1
+}
+
+const standings = calculateStandings(results)
+fs.writeFileSync('standings.json', JSON.stringify(standings, null, 4))
